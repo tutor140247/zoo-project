fe7cc53f051cb285ec7145124c147cd9e0587585
VAROD TATIYATIDSANA <varodtatiyatidsana@gmail.com>
2024-11-14 19:14:00 +0000

    Add test cases

--- test_zoo.py
+++ test_zoo.py
@@ -7,7 +7,17 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+        self.assertEqual(self.zoo.get_ticket_price(-1), None)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+    
+    def test_child2_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_child3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
     # Add your additional test cases here.
 
 if __name__ == '__main__':
